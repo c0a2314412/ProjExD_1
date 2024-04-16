8846d172e2a28561b2e433b90ec5ba7dbb019306
yoshida <c0a2316612@edu.teu.ac.jp>
2024-04-16 16:22:36 +0900

    そらがうごくよ

--- pygame_image.py
+++ pygame_image.py
@@ -16,12 +16,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        if tmr==800:#5
+            tmr=0#5
+        screen.blit(bg_img, [-tmr, 0])#5
         screen.blit(tori,[300,200])#4
         pg.display.update()
+        x+=1
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200)#5
 
 
 if __name__ == "__main__":
