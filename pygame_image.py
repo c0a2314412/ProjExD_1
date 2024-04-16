7795cdbfbf31cd336850365c5dd90edd006cc41d
yoshida <c0a2316612@edu.teu.ac.jp>
2024-04-16 16:31:07 +0900

    そらがうごくよ

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2=pg.image.load("fig/pg_bg.jpg")
     tori=pg.image.load("fig/3.png")#2
     tori=pg.transform.flip(tori,True,False)#2
+    x=0
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        if tmr==800:#5
-            tmr=0#5
-        screen.blit(bg_img, [-tmr, 0])#5
+        x=tmr%800
+        screen.blit(bg_img, [-x, 0])#5
         screen.blit(tori,[300,200])#4
         pg.display.update()
-        x+=1
         tmr += 1        
         clock.tick(200)#5
 
