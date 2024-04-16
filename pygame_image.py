f53b51fe4f7c5c9d580387e1b544f889be60dbb6
yoshida <c0a2316612@edu.teu.ac.jp>
2024-04-16 17:26:35 +0900

    れんしゅうおわり

--- pygame_image.py
+++ pygame_image.py
@@ -15,6 +15,8 @@ def main():
     tori=pg.transform.flip(tori,True,False)#2
     x=0#6
     tmr = 0
+    tori_rct=tori.get_rect()#8-1
+    tori_rct.center=300,200#8-2
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -22,7 +24,16 @@ def main():
         screen.blit(bg_img2,[-x+1600,0])#7-1
         screen.blit(bg_img, [-x, 0])#6
         screen.blit(bg_img,[-x+3200,0])#7-2
-        screen.blit(tori,[300,200])#4
+        key_lst = pg.key.get_pressed()#8-3
+        if key_lst[pg.K_UP]:#8-4
+            tori_rct.move_ip((0,-1))#8-4
+        if key_lst[pg.K_DOWN]:#8-4
+            tori_rct.move_ip((0,1))#8-4
+        if key_lst[pg.K_RIGHT]:#8-4
+            tori_rct.move_ip((1,0))#8-4
+        if key_lst[pg.K_LEFT]:#8-4
+            tori_rct.move_ip((-1,0))#8-4
+        screen.blit(tori,tori_rct)#4 #8-5
         pg.display.update()
         tmr += 1        
         clock.tick(200)#5
